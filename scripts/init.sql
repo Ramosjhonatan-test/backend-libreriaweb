61f583b4ff2db6c0fbcfc2bab25e5fecb24e5d90
Jhonatan <danielacopana@gmail.com>
2025-06-17 21:35:18 -0400

    hola 1

--- scripts/init.sql
+++ scripts/init.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS empleados (
   nombre_completo VARCHAR(100) NOT NULL,
   ci VARCHAR(15) UNIQUE NOT NULL CHECK (ci ~ '^[0-9]{7,10}$'),
   telefono VARCHAR(15) NOT NULL,
-  email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$'
+  email VARCHAR(100)
   direccion TEXT,
   foto_url VARCHAR(255),
   fecha_contratacion DATE NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS proveedores (
   nombre VARCHAR(100) NOT NULL,
   contacto VARCHAR(100) NOT NULL,
   telefono VARCHAR(15) NOT NULL,
-  email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$'
+  email VARCHAR(100),
   direccion TEXT,
   logo_url VARCHAR(255),
   sitio_web VARCHAR(100),
@@ -67,7 +67,7 @@ CREATE TABLE IF NOT EXISTS clientes (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(100) NOT NULL,
   telefono VARCHAR(15),
-  email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$'
+  email VARCHAR(100),
   creado_en TIMESTAMP DEFAULT NOW(),
   actualizado_en TIMESTAMP DEFAULT NOW()
 );
